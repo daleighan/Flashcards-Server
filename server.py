8a4c6190cb86fc0dd71b32c63e2a137e5c3e80de
David Leigh <daleighan@gmail.com>
2017-12-04 16:23:23 -0800

    Initial commit

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import dynamo_info
+from flask_dynamo import Dynamo
+
+app = Flask(__name__)
+
+app.config['DYNAMO_TABLES'] = [
+    {
+         "TableName":"Flashcards",
+         "KeySchema":[dict(AttributeName="id", KeyType='HASH')],
+         "AttributeDefinitions":[dict(AttributeName="username", AttributeType="S"), dict(AttributeName="category", AttributeType="S"), dict(AttributeName="front", type="S"), dict(AttributeName="back", type="S")],
+         "ProvisionedThroughput":dict(ReadCapacityUnits=5, WriteCapacityUnits=5)
+    }
+]
+
+dynamo = Dynamo(app)
+
+@app.route("/")
+def hello_world():
+    return "Hello World"
+
+if __name__ == "__main__":
+    with app.app_context():
+        dynamo.create_all()
+    app.run()
